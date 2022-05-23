d351d4bc2d268a3fb00e15236e8ce584e020bbeb
Revolution5 <damonboone99@gmail.com>
2022-05-23 14:34:13 -0700

    Implement ship hit() and isSunk() with tests

--- src/ship.js
+++ src/ship.js
@@ -1,6 +1,14 @@
 export class Ship {
     constructor(length) {
         this.length = length;
+        this.positions = [];
+        this.sunk = false;
+        for(let i = 0; i < this.length; i++) {
+            //fill the array with booleans initally set to false
+            //tracks the specific position that each ship has been hit in
+            let isHit = false;
+            this.positions.push(isHit);
+        }
     }
 
     get length() {
@@ -9,5 +17,16 @@ export class Ship {
 
     set length(length) {
         this._length = length;
+    }
+
+    hit(position) {
+        this.positions[position] = true;
+    }
+
+    isSunk() {
+        //if every position in the array is true(aka has been hit), the ship has been sunk
+        if(this.positions.every(element => element == true)) {
+            this.sunk = true;
+        }
     }
 }
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -4,3 +4,33 @@ test("ship returns length", () => {
     let testShip = new Ship(4);
     expect(testShip.length).toBe(4);
 })
+
+test("set ship length", () => {
+    let testShip = new Ship(4);
+    testShip.length = 5;
+    expect(testShip.length).toBe(5);
+})
+
+test("positions array is filled correctly", () => {
+    let testShip = new Ship(4);
+    expect(testShip.positions[0]).toBe(false);
+    expect(testShip.positions[1]).toBe(false);
+    expect(testShip.positions[2]).toBe(false);
+    expect(testShip.positions[3]).toBe(false);
+})
+
+test("hitting a ship", () => {
+    let testShip = new Ship(4);
+    testShip.hit(3);
+    expect(testShip.positions[3]).toBe(true);
+})
+
+test("sinking a ship", () => {
+    let testShip = new Ship(4);
+    testShip.hit(0);
+    testShip.hit(1);
+    testShip.hit(2);
+    testShip.hit(3);
+    testShip.isSunk();
+    expect(testShip.sunk).toBe(true);
+})
