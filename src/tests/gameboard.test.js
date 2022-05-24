ad11d62c1b5331397df3c6729ac5732be4a623f9
Revolution5 <damonboone99@gmail.com>
2022-05-23 17:00:11 -0700

    Define the gameboard array and placeShip()

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,1 +1,8 @@
 import { Gameboard } from "../gameboard";
+import { Ship } from "../ship";
+
+test("place a ship at specific coordinates", () => {
+    let testGameboard = new Gameboard;
+    testGameboard.placeShip(0,1,4);
+    expect(testGameboard.board[0][1]).not.toBe("");
+})
