d678434c890f1a83ef4e945d809aa89d6e34b0e1
Revolution5 <damonboone99@gmail.com>
2022-05-24 12:42:09 -0700

    Change gameboard to be filled with nulls

--- src/gameboard.js
+++ src/gameboard.js
@@ -4,22 +4,25 @@ export class Gameboard {
     constructor() {
         this.board = [
             //10 x 10 grid
-            //0,  1,  2,  3,  4,  5,  6,  7,  8,  9
-            ["", "", "", "", "", "", "", "", "", ""],//0
-            ["", "", "", "", "", "", "", "", "", ""],//1
-            ["", "", "", "", "", "", "", "", "", ""],//2
-            ["", "", "", "", "", "", "", "", "", ""],//3
-            ["", "", "", "", "", "", "", "", "", ""],//4
-            ["", "", "", "", "", "", "", "", "", ""],//5
-            ["", "", "", "", "", "", "", "", "", ""],//6
-            ["", "", "", "", "", "", "", "", "", ""],//7
-            ["", "", "", "", "", "", "", "", "", ""],//8
-            ["", "", "", "", "", "", "", "", "", ""] //9
+            //0,    1,    2,    3,    4,    5,    6,    7,    8,    9
+            [null, null, null, null, null, null, null, null, null, null],//0
+            [null, null, null, null, null, null, null, null, null, null],//1
+            [null, null, null, null, null, null, null, null, null, null],//2
+            [null, null, null, null, null, null, null, null, null, null],//3
+            [null, null, null, null, null, null, null, null, null, null],//4
+            [null, null, null, null, null, null, null, null, null, null],//5
+            [null, null, null, null, null, null, null, null, null, null],//6
+            [null, null, null, null, null, null, null, null, null, null],//7
+            [null, null, null, null, null, null, null, null, null, null],//8
+            [null, null, null, null, null, null, null, null, null, null] //9
         ]
     }
 
     placeShip(xPos, yPos, length) {
-        this.board[xPos][yPos] = new Ship(length);
+        if(this.board[xPos][yPos] == null) {
+           this.board[xPos][yPos] = new Ship(length); 
+        }
+        
     }
 
     receiveAttack(xPos, yPos) {
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -4,5 +4,5 @@ import { Ship } from "../ship";
 test("place a ship at specific coordinates", () => {
     let testGameboard = new Gameboard;
     testGameboard.placeShip(0,1,4);
-    expect(testGameboard.board[0][1]).not.toBe("");
+    expect(testGameboard.board[0][1]).not.toBe(null);
 })
