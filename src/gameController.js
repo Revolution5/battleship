bcc550a429bfa194f40c40725ca0cf28564cb9ea
Revolution5 <damonboone99@gmail.com>
2022-05-27 15:34:39 -0700

    Change css for squares if hit/has ship

--- src/gameController.js
+++ src/gameController.js
@@ -11,15 +11,31 @@ function populatePlayerBoard() {
 
     for(let i = 0; i < 10; i++) {
         for(let j = 0; j < 10; j++) {
+            //no ship no hit
             if(human.gameboard.board[i][j].ship == null && human.gameboard.board[i][j].isHit == false) {
                 let square = document.createElement("div");
                 square.classList.add("square");
                 square.setAttribute("id", i.toString() + j.toString());
                 humanGrid.appendChild(square);   
             }
+            //yes ship no hit
             else if(human.gameboard.board[i][j].ship != null && human.gameboard.board[i][j].isHit == false) {
                 let square = document.createElement("div");
                 square.classList.add("square-ship");
+                square.setAttribute("id", i.toString() + j.toString());
+                humanGrid.appendChild(square);
+            }
+            //no ship yes hit
+            else if(human.gameboard.board[i][j].ship == null && human.gameboard.board[i][j].isHit == true) {
+                let square = document.createElement("div");
+                square.classList.add("square-hit");
+                square.setAttribute("id", i.toString() + j.toString());
+                humanGrid.appendChild(square);
+            }
+            //yes ship yes hit
+            else if(human.gameboard.board[i][j].ship != null && human.gameboard.board[i][j].isHit == true) {
+                let square = document.createElement("div");
+                square.classList.add("square-ship-hit");
                 square.setAttribute("id", i.toString() + j.toString());
                 humanGrid.appendChild(square);
             }
@@ -32,11 +48,34 @@ function populateEnemyBoard() {
 
     for(let i = 0; i < 10; i++) {
         for(let j = 0; j < 10; j++) {
+            //no ship no hit
             if(computer.gameboard.board[i][j].ship == null && computer.gameboard.board[i][j].isHit == false) {
                 let square = document.createElement("div");
                 square.classList.add("square");
                 square.setAttribute("id", i.toString() + j.toString());
                 computerGrid.appendChild(square);   
+            }
+            //yes ship no hit 
+            //computer ship must be invisible to the human player before being hit
+            else if(computer.gameboard.board[i][j].ship != null && computer.gameboard.board[i][j].isHit == false) {
+                let square = document.createElement("div");
+                square.classList.add("square");
+                square.setAttribute("id", i.toString() + j.toString());
+                computerGrid.appendChild(square); 
+            }
+            //no ship yes hit
+            else if(computer.gameboard.board[i][j].ship == null && computer.gameboard.board[i][j].isHit == true) {
+                let square = document.createElement("div");
+                square.classList.add("square-hit");
+                square.setAttribute("id", i.toString() + j.toString());
+                computerGrid.appendChild(square); 
+            }
+            //yes ship yes hit
+            else if(computer.gameboard.board[i][j].ship != null && computer.gameboard.board[i][j].isHit == true) {
+                let square = document.createElement("div");
+                square.classList.add("square-ship-hit");
+                square.setAttribute("id", i.toString() + j.toString());
+                computerGrid.appendChild(square); 
             }
         }
     }
@@ -49,12 +88,16 @@ function clearBoard(board) {
 }
 
 export function startGame() {
+    human.gameboard.placeShip(2,3,5);
+    human.gameboard.placeShip(0,1,4);
+    human.gameboard.placeShip(4,4,3);
+    human.gameboard.placeShip(6,7,3);
+    human.gameboard.placeShip(8,1,2);
+
+    computer.gameboard.placeShip(0,1,2);
+    
     populatePlayerBoard();
     populateEnemyBoard();
-    
-    human.gameboard.placeShip(0,1,4);
-    human.gameboard.placeShip(2,3,5);
-    populatePlayerBoard();
 }
 
 
