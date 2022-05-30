4aa63fe009f24778b675ab224c92e9f595e62fbd
Revolution5 <damonboone99@gmail.com>
2022-05-30 13:14:28 -0700

    Add winner text and labels for each board

--- src/gameController.js
+++ src/gameController.js
@@ -10,25 +10,26 @@ export function clickHandler(e) {
     let x = clicked[0];
     let y = clicked[1];
     if(computer.gameboard.board[x][y].isHit == false) {
-        human.takeTurn(computer.gameboard, x, y);
+        human.takeTurn(computer.gameboard, x, y)
         populateEnemyBoard();
-        checkForWin(); 
         computer.takeTurnAI(human.gameboard);
         populatePlayerBoard(); 
+        checkForWin();
     }
 }
 
 function checkForWin() {
     let computerSquares = Array.from(document.querySelectorAll(".computer-grid > div"));
+    let winnerText = document.querySelector(".winner-text > h1");
 
     if(computer.gameboard.allShipsSunk()) {
-        console.log("You Win!");
+        winnerText.textContent = "You Win!"
         computerSquares.forEach(square => {
             square.removeEventListener("click", clickHandler);
         })
     }
     else if(human.gameboard.allShipsSunk()) {
-        console.log("You Lose!");
+        winnerText.textContent = "You Lose!"
         computerSquares.forEach(square => {
             square.removeEventListener("click", clickHandler);
         })
@@ -36,10 +37,10 @@ function checkForWin() {
 }
 
 export function startGame() {
-    human.gameboard.placeShip(2,3,5);
-    human.gameboard.placeShip(0,1,4);
-    human.gameboard.placeShip(4,4,3);
-    human.gameboard.placeShip(6,7,3);
+    // human.gameboard.placeShip(2,3,5);
+    // human.gameboard.placeShip(0,1,4);
+    // human.gameboard.placeShip(4,4,3);
+    // human.gameboard.placeShip(6,7,3);
     human.gameboard.placeShip(8,1,2);
 
     computer.gameboard.placeShip(0,1,2);
